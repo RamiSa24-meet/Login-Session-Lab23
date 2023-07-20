82d22d901376fff6fb31b020fd4a82b2263fe07c
RamiSa24-meet <139688880+RamiSa24-meet@users.noreply.github.com>
2023-07-20 18:11:37 +0300

    don with the bonus

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -4,10 +4,26 @@ from flask import session as login_session
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-@app.route('/', ) # What methods are needed?
-def home():
+
 	
-	return render_template('home.html')
+@app.route('/',methods=['GET', 'POST'])  # '/' for the default page
+def login():
+  if request.method == 'GET':
+    return render_template('home.html')
+  else:
+    try:
+      z =request.form['Number'] 
+      x = request.form['Name']
+      y = request.form['Quote']
+      login_session['Age']= z
+      login_session['Quote']=y 
+      login_session['Name']= x
+      return render_template('thanks.html',login_session = login_session)
+    except:
+       return render_template('error.html')
+
+    
+	      
 
 
 @app.route('/error')
@@ -18,8 +34,7 @@ def error():
 
 @app.route('/display')
 def display():
-
-	return render_template('display.html', ) # What variables are needed?
+	return render_template('display.html',login_session = login_session ) # What variables are needed?
 
 
 @app.route('/thanks')
